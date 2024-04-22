43aca65fe6b14995b84a0345e2c3e9e13837c7d2
AdanLedesma66 <adagalese@gmail.com>
2024-04-21 22:24:37 -0400

    exceptions

--- src/main/java/py/com/template/application/engine/EngineTest.java
+++ src/main/java/py/com/template/application/engine/EngineTest.java
@@ -2,6 +2,6 @@ package py.com.template.application.engine;
 
 public class EngineTest {
 
-    // ::: todo crear response para el controller y buen mapeo para el manejo de excepciones
+    // ::: todo crear response para el controller y buen mapeo para el manejo de excepciones, tambien sobrecarga de metodos para el mismo
 
 }
